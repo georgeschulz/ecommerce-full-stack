e71dd427135b9de3c53ced1c5296125bd4329ae5
georgeschulz <georgeschulz33@gmail.com>
2022-08-27 15:23:30 -0400

    Connect get detailed service to front end

--- frontend/src/api/getServices.js
+++ frontend/src/api/getServices.js
@@ -10,3 +10,7 @@ export const getServices = async (target, programType, warranty) => {
 export const getDetailedServiceInfo = async (customerId, target) => {
     return await axios.get(`${endpoint}/services/detail/user/${customerId}?target=${target}`);
 }
+
+export const getDetailedServiceInfoByServiceId = async (customerId, target, serviceId) => {
+    return await axios.get(`${endpoint}/services/detail/user/${customerId}/service/${serviceId}?target=${target}`)
+}
--- frontend/src/pages/serviceDetail/serviceDetail.js
+++ frontend/src/pages/serviceDetail/serviceDetail.js
@@ -1,11 +1,27 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getDetailedServiceInfoByServiceId } from "../../api/getServices";
+import { useSelector } from 'react-redux';
+import { selectSelectedPest } from "../../features/wizardSlice";
+import { selectUserId } from "../../features/auth";
 
 function ServiceDetail() {
     const { serviceId } = useParams();
+    const userId = useSelector(selectUserId);
+    const target = useSelector(selectSelectedPest);
+    const [service, setService] = useState(null);
+
+
+    useEffect(() => {
+        const getService = async () => {
+            const response = await getDetailedServiceInfoByServiceId(userId, target, serviceId);
+            setService(response.data)
+        }
+        getService();
+    }, [])
 
     return (
-        <p>Service details</p>
+        <p>Service details:</p>
     )
 }
 
