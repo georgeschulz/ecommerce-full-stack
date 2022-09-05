7b84b0154c6e2223cc1978dc1593c622bc168705
georgeschulz <georgeschulz33@gmail.com>
2022-09-05 16:24:03 -0400

    Fix rerender bug

--- frontend/src/components/buttons/bookButton.js
+++ frontend/src/components/buttons/bookButton.js
@@ -5,18 +5,20 @@ import { useNavigate } from "react-router-dom";
 import { selectUserId } from "../../features/auth";
 import { useSelector } from "react-redux";
 import { setAppointmentDate } from "../../api/schedule";
+import { selectMostRecentItem } from "../../features/cart";
 
 function BookButton({date, routeId}) {
     const [link, setLink] = useState('/wizard/5');
     const navigate = useNavigate();
     const userId = useSelector(selectUserId);
+    const cartChange = useSelector(selectMostRecentItem)
 
     useEffect(() => {
         (async () => {
             const response = await getStripeLink(userId, date);
             setLink(response.data)
         })();
-    }, [])
+    }, [cartChange])
 
     const handleClick = async () => {
         const response = await setAppointmentDate(routeId, userId);
