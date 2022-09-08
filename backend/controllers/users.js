412c2a1de15430ea7a3c2d80d7bb19de0a359403
georgeschulz <georgeschulz33@gmail.com>
2022-09-07 22:43:04 -0400

    Updates to error handling in users route

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,34 +2,37 @@ const queries = require('../queries');
 const db = require('../db');
 
 //get all of the users
-const getUsers = (req, res) => {
-    db.query(queries.getUsers, (err, results) => {
-        if(err) throw(err);
-        res.status(200).send(results.rows);
-    })
+const getUsers = async (req, res) => {
+    try {
+        const usersQuery = await db.query(queries.getUsers);
+        const users = usersQuery.rows;
+        res.status(200).send(users);
+    } catch (err) {
+        console.log(err);
+        res.status(404).send('There was an error getting a list of users');
+    }
 }
 
 //get a specific user by the customer_id
-const getUserById = (req, res) => {
-    const id = req.user.customer_id;
-    db.query(queries.getUserById, [id], (err, results) => {
-        if(err) {
-            res.status(404).send({msg: 'Could not find user'});
-        } else if (results.rows.length <= 0) {
-            res.status(404).send({users: []}); //send an error when no customers are found for that ID
-        } else {
-            res.send({users: results.rows});
-        }
-    })
+const getUserById = async (req, res) => {
+    try {
+        const id = req.user.customer_id;
+        const userQuery = await db.query(queries.getUserById, [id]);
+        const user = userQuery.rows;
+        res.status(200).send({users: user})
+
+    } catch (err) {
+        console.log(err);
+        res.status(404).send('There was an error finding the user');
+    }
 }
 
 const updateUser = async (req, res) => {
-    const id = req.user.customer_id;
-    const { firstName, lastName, address, city, state, zip, email, phone, squareFeet} = req.body; //get the parameters to update from the request
-    let area_id = 1;
-
     //get the area id to make sure scheduling works properly down the road
     try {
+        const id = req.user.customer_id;
+        const { firstName, lastName, address, city, state, zip, email, phone, squareFeet} = req.body; //get the parameters to update from the request
+        let area_id = 1;
         const areaIdQuery = await db.query(queries.getAreaId, [city]);
         area_id = areaIdQuery.rows[0].area_id;
 
@@ -45,15 +48,12 @@ const updateUser = async (req, res) => {
             squareFeet,
             area_id,
             id
-        ])
+        ]);
+        res.status(200).send({msg: 'Successful update to the user'})
     } catch (err) {
         console.log(err)
         res.status(400).send({msg: 'Error updating the account'})   
-   
     }
-
-
-    res.status(200).send({msg: 'Successful update to the user'})
 }
 
 module.exports = {
