4fd28ebdd82627af6a9b35df78b6e1ce3a6fa037
georgeschulz <georgeschulz33@gmail.com>
2022-09-12 21:31:53 -0400

    Fix api error with path in config file

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,4 +1,4 @@
 export const endpoint =
   process.env.NODE_ENV === "production"
-    ? "https://pest-control-ecommerce.herokuapp.com/"
+    ? "https://pest-control-ecommerce.herokuapp.com"
     : "http://localhost:4000";
