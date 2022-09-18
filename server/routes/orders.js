c9e7525f20085d9de721ba08d5e9f62f68ddd965
georgeschulz <georgeschulz33@gmail.com>
2022-09-18 19:41:05 -0400

    Protect order retrieval route

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -4,7 +4,7 @@ const controllers = require('../controllers/orders');
 const checkIsAuthenticatedAdmin = require('../helpers/checkIsAuthenticatedAdmin')
 
 router.get("/", checkIsAuthenticatedAdmin, controllers.getAllOrders);
-router.get("/:order_id", checkIsAuthenticatedAdmin, controllers.getOrderById);
+router.get("/:order_id", checkIsAuthenticatedAdmin,controllers.getOrderById);
 router.get("/stripe/:stripeSession", controllers.getOrderByStripeSession)
 
 module.exports = router;
